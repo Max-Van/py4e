054a413cb6626cd7507068c9fa70f272345ccff4
max <maxffq@gmail.com>
2019-04-16 22:55:38 +0800

    update chapter11

--- chapter B - Regular Expression/ex_11_00.py
+++ chapter B - Regular Expression/ex_11_00.py
@@ -7,17 +7,16 @@ a=re.search('fan',string)
 #print(a)
 
 #  ^X.*:
-fh=open('/Users/max/Develop/Coursera/py4egit/chapter7 - file/mbox-short.txt')
+#fh=open('/Users/max/Develop/Coursera/py4egit/chapter7 - file/mbox-short.txt')
 
 #for line in fh:
 #    if re.search('^X.*:',line):
 #        print(line)
 #  ^X-\S+*:
-for line in fh:
-    if re.search('^X-\S+:',line):
-        print(line)
+#for line in fh:
+#    if re.search('^X-\S+:',line):
+#        print(line)
 
-str='max12 fan3 hello548'
-
-fl=re.findall('[0-9]+',str)
-print(fl)
+x = 'From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008'
+y = re.findall('\S+?@\S+', x)
+print(y)
