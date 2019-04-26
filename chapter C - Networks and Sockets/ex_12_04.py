b14d4c868702126ac54a18705586d3019c1c3f22
max <maxffq@gmail.com>
2019-04-26 12:41:54 +0800

    update chapter C

--- chapter C - Networks and Sockets/ex_12_04.py
+++ chapter C - Networks and Sockets/ex_12_04.py
@@ -1,6 +1,8 @@
 import urllib.request,urllib.parse,urllib.error
 
 fhand=urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+#fhand=urllib.request.urlopen('http://www.apple.com')
+
 
 for line in fhand:
     print(line.decode().strip())
